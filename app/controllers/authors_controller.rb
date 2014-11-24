d1143d1acb1be54f5589f7b3f5cc4967604c9867
Adam Bray <adam.bray@yale.edu>
2014-11-24 18:15:26 -0500

    updated to day 2 starter code

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -10,44 +10,6 @@ class AuthorsController < ApplicationController
     @author = Author.find(params[:id])
   end
 
-  # GET /authors/new
-  def new
-    @author = Author.new
-  end
-
-  # GET /authors/1/edit
-  def edit
-    @author = Author.find(params[:id])
-  end
-
-  # POST /authors
-  def create
-    @author = Author.new( first_name: params[:author][:first_name],
-                          last_name: params[:author][:last_name],
-                          nationality: params[:author][:nationality],
-                          birth_year: params[:author][:birth_year])
-
-    if @author.save
-      redirect_to @author
-    else
-      render :new
-    end
-  end
-
-  # PATCH/PUT /authors/1
-  def update
-    @author = Author.find(params[:id])
-
-    if @author.update( first_name: params[:author][:first_name], 
-                       last_name: params[:author][:last_name],
-                       nationality: params[:author][:nationality],
-                       birth_year: params[:author][:birth_year])
-      redirect_to @author
-    else
-      render :edit
-    end
-  end
-
   # DELETE /authors/1
   def destroy
     @author = Author.find(params[:id])
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,43 +10,6 @@ class BooksController < ApplicationController
     @author = Book.find(params[:id])
   end
 
-  # GET /books/new
-  def new
-    @author = Book.new
-  end
-
-  # GET /books/1/edit
-  def edit
-    @author = Book.find(params[:id])
-  end
-
-  # POST /books
-  def create
-    @author = Book.new( title: params[:book][:title],
-                        publication_date: params[:book][:publication_date],
-                        author_id: params[:book][:author_id] )
-
-    if @author.save
-      redirect_to @author
-    else
-      render :new
-    end
-  end
-
-  # PATCH/PUT /books/1
-  def update
-    @author = Book.find(params[:id])
-
-    if @author.update( title: params[:book][:title],
-                       publication_date: params[:book][:publication_date],
-                       author_id: params[:book][:author_id] )
-
-      redirect_to @author
-    else
-      render :edit
-    end
-  end
-
   # DELETE /books/1
   def destroy
     @author = Book.find(params[:id])
